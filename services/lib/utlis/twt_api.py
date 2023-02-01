16c466b6539d87defad986bc0c1ff682d1617220
akp10110 <akp10110@gmail.com>
2023-02-01 23:17:57 +1100

    tweet timeline

--- services/lib/utlis/twt_api.py
+++ services/lib/utlis/twt_api.py
@@ -16,3 +16,11 @@ def get_followers_of_user_id(user_id: str, auth_token: str):
     response = requests.get(url, headers=headers)
     response_json = json.loads(response.text)
     return response_json
+
+
+def get_user_tweet_timeline_by_id(user_id: str, auth_token: str):
+    url = f'https://api.twitter.com/2/users/{user_id}/tweets'
+    headers = {'authorization': f'Bearer {auth_token}'}
+    response = requests.get(url, headers=headers)
+    response_json = json.loads(response.text)
+    return response_json
